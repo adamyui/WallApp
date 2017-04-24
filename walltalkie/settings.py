0d06c6d0d346919002775039a24dcda732025486
radamizell <radamizell@gmail.com>
2017-04-24 18:27:40 -0400

    settings.py database pw change

--- walltalkie/settings.py
+++ walltalkie/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'dr0ngletr0n',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
